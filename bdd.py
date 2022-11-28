e97768d95d8a020eb283fd7c3f37f98d694a54f4
marcesalas96 <marcesalas1996@gmail.com>
2022-11-27 23:08:45 -0300

    Nuevo archivo para creacion de base de datos sqlite3, tablas: Usuario, Sala, Pelicula, Funcion y Butaca

---
+++ bdd.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+conexion = sqlite3.connect("cinemar.sqlite3")
+cursor = conexion.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS usuario (
+                id_usuario integer primary key autoincrement, nombre_usuario TEXT NOT NULL, 
+                contraseña TEXT NOT NULL,
+                correo text not null,
+                admin BOOLEAN,
+                nombre TEXT NOT NULL,
+                apellido TEXT NOT NULL,
+                edad INTEGER NOT NULL,
+                tiene_tarjeta boolean not null)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS sala(
+                id_sala integer primary key autoincrement,
+                cantidad_butacas integer not null)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS pelicula(
+                id_pelicula integer PRIMARY KEY autoincrement,
+                titulo text,
+                duracion int,
+                idioma text,
+                edad integer,
+                descripcion text,
+                genero text,
+                tipo text not null,
+                id_sala integer,
+                FOREIGN KEY(id_sala) REFERENCES sala(id_sala))""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS funcion (
+                id_funcion integer PRIMARY KEY autoincrement,
+                horario text,
+                fecha text,
+                id_sala integer
+                FOREIGN KEY(id_sala) REFERENCES sala(id_sala))""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS butaca(
+                pos integer primary key,
+                ocupado boolean,
+                id_funcion integer
+                FOREIGN KEY(id_funcion) REFERENCES funcion(id_funcion)
+                """)
+conexion.commit()
+conexion.close()
